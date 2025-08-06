a9d5882121126a4bfd792aca1e360f6c9a721ffb
Medhat <elsa0099@algonquinlive.com>
2025-08-06 12:05:58 -0400

    Fixed missing closing li tag in header.

--- Common/Header.php
+++ Common/Header.php
@@ -46,6 +46,7 @@ if (session_status() == PHP_SESSION_NONE) {
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="EditProfile.php">Edit Profile</a>
+                    </li>
                     <li class="nav-item ms-auto me-2">
                         <?php if (isset($_SESSION['user'])): ?>
                             <a class="nav-link" href="Logout.php">Log Out</a>
